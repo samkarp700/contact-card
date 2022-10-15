2c9fb6bfe45621f664490d16c67ca7001a474670
Sam Karp <sfcypret@gmail.com>
2022-10-14 20:39:26 -0400

    cards working properly

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,9 +1,9 @@
 //entry point that webpack will look at to determine dependencies and assets for app
-import "./form";
-import "./submit";
+import { toggleForm, clearForm } from "./form";
 //import bootstrap and popper
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {fetchCards} from './cards';
 
 
 //import css files
@@ -18,12 +18,44 @@ import { getDB, initDB, postDB } from '../js/database';
 
 window.addEventListener('load', function() {
     initDB();
-    getDB();
-    postDB();
-    getDB();
+    fetchCards();
     document.getElementById('logo').src = Logo;
-    document.getElementById('bear').src = Bear;
-    document.getElementById('dog').src = Dog;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
 })
 
-
+  // Form functionality
+  const form = document.getElementById("formToggle");
+  const newContactButton = document.getElementById("new-contact");
+  let submitBtnToUpdate = false;
+  let profileId;
+  
+  newContactButton.addEventListener('click', event => {
+    toggleForm()
+   })
+  
+  form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+  
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDB(name, email, phone, profile);
+  } else {
+  
+    fetchCards();
+      // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+  
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+  });
